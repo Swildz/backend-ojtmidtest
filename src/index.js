a9483c3c7d1641129477cc088bec4832a3934687
Swildz <satupercobaan12@gmail.com>
2023-05-22 23:34:06 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -27,13 +27,3 @@ app.use(express.static(__dirname))
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-main().catch(console.error);
