b7f474574be47f7594d35cf046630925463518ff
Swildz <satupercobaan12@gmail.com>
2023-05-29 23:18:08 +0530

    update deployment

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -1,8 +1,8 @@
 # #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # . ~/.nvm/nvm.sh
-nvm install node
+# nvm install node
 # hahaha
 # buat directory jika directory tidak ada
 DIR="/home/ec2-user/mahasiswa"
