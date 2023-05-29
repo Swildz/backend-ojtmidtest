515157e69c5c43889a24112ce09747739493d8fc
Swildz <satupercobaan12@gmail.com>
2023-05-29 22:25:19 +0530

    update deployment

--- deploy_scripts/application_start.sh
+++ deploy_scripts/application_start.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+# menjalankan perintah sebagai admin(sementara) untuk mengelola directory
+sudo chmod -R 777 /home/ec2-user/store
 
-DIR="$HOME/express-app"
-cd $DIR
+# pindah ke directory
+cd /home/ec2-user/store
 
+# add npm and node to path
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+# install node modul
 npm install
+
+# start node app ind the background
 node ./src/index.js > app.out.log 2> app.err.log < /dev/null &
